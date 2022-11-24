a7301d212e38fe299aff4c2422c93bf6887fc174
xufengchun <xufengchun09@gmail.com>
2022-11-24 18:22:58 +0800

    Initial commit

--- day.py
+++ day.py
@@ -16,4 +16,9 @@ plt.title('Histogram of IQ')
 plt.text(60, .025, r'$\mu=100,\ \sigma=15$')
 plt.axis([40, 160, 0, 0.03])
 plt.grid(True)
+<<<<<<< HEAD
 plt.show()
+=======
+plt.show()
+print("hello")
+>>>>>>> 9042a89 (Initial commit)
